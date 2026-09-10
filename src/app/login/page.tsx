a8abea0b8a879d8e4f566e578eb24a3cc8f37a3c
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2026-09-10 22:34:23 +0600

    inital setup for frontend using next js

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+
+
+const LoginPage = () => {
+    return (
+        <div>
+           <h1 className="text-2xl text-center my-5 text-red-500">Login page here</h1> 
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+
+
+const RegisterPage = () => {
+    return (
+        <div>
+           <h1 className="text-2xl text-center my-5 text-red-500">Register page here</h1> 
+        </div>
+    );
+};
+
+export default RegisterPage;
